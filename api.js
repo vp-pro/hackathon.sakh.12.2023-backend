38d163e50781baf22496324b97ff89adb63b8ef2
Vladislav Panferov <panfyor26@gmail.com>
2023-12-17 11:21:39 +1100

    Final Commit

---
+++ api.js
@@ -0,0 +1,5 @@
+module.exports = {
+    functional_sentiment: '../python/functional_sentiment.py',
+    emotional_sentiment: '../python/emotional_sentiment.py',
+    language_detection: '../python/language_detection.py'
+  };
--- app.js
+++ app.js
@@ -1,137 +1,225 @@
+// Подключаем необходимые модули и скрипты
+const { functional_sentiment, emotional_sentiment, language_detection } = require('./api');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { spawn } = require('child_process');
 const cors = require('cors');
 
+// Создаем экземпляр приложения Express
 const app = express();
 const port = 3001;
-app.use(cors()); // Enable CORS
 
+// Включаем поддержку CORS
+app.use(cors());
+
+// Используем парсер JSON для обработки запросов с телом в формате JSON
 app.use(bodyParser.json());
 
+// Обработчик для корневого маршрута
 app.get('/', (req, res) => {
   res.send('Hello, welcome to your Node.js Express API!');
 });
 
+// Обработчик для маршрута /v1/sentiment
 app.post('/v1/sentiment', (req, res) => {
-  const texts = req.body.message;
-  
-  const pythonProcess = spawn('python3', ['sentiment.py', ...texts]);
+  const texts = req.body.messages;
 
-  let output = '';
+  // Детекция языка
+  const pythonLanguageDetection = spawn('python3', [language_detection, ...texts]);
+  let languageOutput = '';
   let errorOutput = '';
 
-  pythonProcess.stdout.on('data', (data) => {
-    output += data.toString();
+  pythonLanguageDetection.stdout.on('data', (data) => {
+    languageOutput += data.toString();
   });
 
-  pythonProcess.stderr.on('data', (data) => {
+  pythonLanguageDetection.stderr.on('data', (data) => {
     errorOutput += data.toString();
   });
 
-  pythonProcess.on('close', (code) => {
-    if (code === 0) {
-      try {
-        const results = JSON.parse(output);
-        res.json(results);
-      } catch (error) {
-        console.error(`Error parsing JSON: ${error}`);
-        res.status(500).send('Internal Server Error');
-      }
-    } else {
-      // Check if the errorOutput contains only the warning message
-      if (errorOutput.includes("Warning : `load_model` does not return")) {
-        // Ignore the warning and return success
-        res.json({ message: 'Success' });
-      } else {
-        console.error(`Python script exited with code ${code}: ${errorOutput}`);
-        res.status(500).send('Internal Server Error');
-      }
+  pythonLanguageDetection.on('close', (code) => {
+    if (code !== 0) {
+      console.error(`Language Detection Python script exited with code ${code}: ${errorOutput}`);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    try {
+      const languageResults = JSON.parse(languageOutput);
+
+      // Функциональный и эмоциональный анализ настроений
+      const pythonFunctional = spawn('python3', [functional_sentiment, ...texts]);
+      const pythonEmotional = spawn('python3', [emotional_sentiment, ...texts]);
+
+      let functionalOutput = '';
+      let emotionalOutput = '';
+      let errorOutput = '';
+
+      pythonFunctional.stdout.on('data', (data) => {
+        functionalOutput += data.toString();
+      });
+
+      pythonFunctional.stderr.on('data', (data) => {
+        errorOutput += data.toString();
+      });
+
+      pythonFunctional.on('close', (functionalCode) => {
+        if (functionalCode !== 0) {
+          console.error(`Functional Python script exited with code ${functionalCode}: ${errorOutput}`);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+
+        pythonEmotional.stdout.on('data', (data) => {
+          emotionalOutput += data.toString();
+        });
+
+        pythonEmotional.stderr.on('data', (data) => {
+          errorOutput += data.toString();
+        });
+
+        pythonEmotional.on('close', (emotionalCode) => {
+          if (emotionalCode !== 0) {
+            console.error(`Emotional Python script exited with code ${emotionalCode}: ${errorOutput}`);
+            res.status(500).send('Internal Server Error');
+            return;
+          }
+
+          const functionalResults = JSON.parse(functionalOutput);
+          const emotionalResults = JSON.parse(emotionalOutput);
+
+          const combinedResults = functionalResults.map((functional, index) => ({
+            message: texts[index],
+            functional_sentiment: functional,
+            emotional_sentiment: emotionalResults[index],
+            detected_language: languageResults[index], // Включаем обнаруженный язык
+          }));
+
+          res.json(combinedResults);
+        });
+      });
+    } catch (error) {
+      console.error(`Error parsing JSON: ${error}`);
+      res.status(500).send('Internal Server Error');
     }
   });
 });
 
+// Обработчик для маршрута /v1/functional_sentiment
+app.post('/v1/functional_sentiment', (req, res) => {
+  const texts = req.body.messages;
 
-app.get('/v1/test', (req, res) => {
-  const texts = ['А вот это первый текст', 'А это другой текст!'];
-
-  const pythonProcess = spawn('python3', ['sentiment.py', ...texts]);
-
-  let output = '';
+  // Функциональный анализ настроений
+  const pythonFunctional = spawn('python3', [functional_sentiment, ...texts]);
+  let functionalOutput = '';
   let errorOutput = '';
 
-  pythonProcess.stdout.on('data', (data) => {
-    output += data.toString();
+  pythonFunctional.stdout.on('data', (data) => {
+    functionalOutput += data.toString();
   });
 
-  pythonProcess.stderr.on('data', (data) => {
+  pythonFunctional.stderr.on('data', (data) => {
     errorOutput += data.toString();
   });
 
-  pythonProcess.on('close', (code) => {
-    if (code === 0) {
-      try {
-        const results = JSON.parse(output);
-        res.json(results);
-      } catch (error) {
-        console.error(`Error parsing JSON: ${error}`);
-        res.status(500).send('Internal Server Error');
-      }
-    } else {
-      // Check if the errorOutput contains only the warning message
-      if (errorOutput.includes("Warning : `load_model` does not return")) {
-        // Ignore the warning and return success
-        res.json({ message: 'Success' });
-      } else {
-        console.error(`Python script exited with code ${code}: ${errorOutput}`);
-        res.status(500).send('Internal Server Error');
-      }
+  pythonFunctional.on('close', (code) => {
+    if (code !== 0) {
+      console.error(`Functional Python script exited with code ${code}: ${errorOutput}`);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    try {
+      const functionalResults = JSON.parse(functionalOutput);
+      const combinedResults = functionalResults.map((functional, index) => ({
+        message: texts[index],
+        functional_sentiment: functional,
+      }));
+      res.json(combinedResults);
+    } catch (error) {
+      console.error(`Error parsing JSON: ${error}`);
+      res.status(500).send('Internal Server Error');
     }
   });
 });
 
+// Обработчик для маршрута /v1/emotional_sentiment
+app.post('/v1/emotional_sentiment', (req, res) => {
+  const texts = req.body.messages;
 
+  // Эмоциональный анализ настроений
+  const pythonEmotional = spawn('python3', [emotional_sentiment, ...texts]);
+  let emotionalOutput = '';
+  let errorOutput = '';
 
-
-app.get('/v1/test', (req, res) => {
-    const texts = ['А вот это первый текст', 'А это другой текст!'];
-  
-    const pythonProcess = spawn('python3', ['sentiment.py', ...texts]);
-  
-    let output = '';
-    let errorOutput = '';
-  
-    pythonProcess.stdout.on('data', (data) => {
-      output += data.toString();
-    });
-  
-    pythonProcess.stderr.on('data', (data) => {
-      errorOutput += data.toString();
-    });
-  
-    pythonProcess.on('close', (code) => {
-      if (code === 0) {
-        try {
-          const results = JSON.parse(output);
-          res.json(results);
-        } catch (error) {
-          console.error(`Error parsing JSON: ${error}`);
-          res.status(500).send('Internal Server Error');
-        }
-      } else {
-        // Check if the errorOutput contains only the warning message
-        if (errorOutput.includes("Warning : `load_model` does not return")) {
-          // Ignore the warning and return success
-          res.json({ message: 'Success' });
-        } else {
-          console.error(`Python script exited with code ${code}: ${errorOutput}`);
-          res.status(500).send('Internal Server Error');
-        }
-      }
-    });
+  pythonEmotional.stdout.on('data', (data) => {
+    emotionalOutput += data.toString();
   });
 
-  
+  pythonEmotional.stderr.on('data', (data) => {
+    errorOutput += data.toString();
+  });
+
+  pythonEmotional.on('close', (code) => {
+    if (code !== 0) {
+      console.error(`Emotional Python script exited with code ${code}: ${errorOutput}`);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    try {
+      const emotionalResults = JSON.parse(emotionalOutput);
+      const combinedResults = emotionalResults.map((emotional, index) => ({
+        message: texts[index],
+        emotional_sentiment: emotional,
+      }));
+      res.json(combinedResults);
+    } catch (error) {
+      console.error(`Error parsing JSON: ${error}`);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+});
+
+// Обработчик для маршрута /v1/language_detection
+app.post('/v1/language_detection', (req, res) => {
+  const texts = req.body.messages;
+
+  // Детекция языка
+  const pythonLanguageDetection = spawn('python3', [language_detection, ...texts]);
+  let languageOutput = '';
+  let errorOutput = '';
+
+  pythonLanguageDetection.stdout.on('data', (data) => {
+    languageOutput += data.toString();
+  });
+
+  pythonLanguageDetection.stderr.on('data', (data) => {
+    errorOutput += data.toString();
+  });
+
+  pythonLanguageDetection.on('close', (code) => {
+    if (code !== 0) {
+      console.error(`Language Detection Python script exited with code ${code}: ${errorOutput}`);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    try {
+      const languageResults = JSON.parse(languageOutput);
+      const combinedResults = languageResults.map((language, index) => ({
+        message: texts[index],
+        detected_language: language,
+      }));
+      res.json(combinedResults);
+    } catch (error) {
+      console.error(`Error parsing JSON: ${error}`);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+});
+
+// Запускаем сервер на указанном порту
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
